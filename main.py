945e11a7ac1081e5b88d2d7277cc5cbe6ad492df
Alperen <alperenmimarlar@gmail.com>
2024-11-09 18:51:33 +0300

    euclideanDistance

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+
+points = [(1, 2), (4, 6), (7, 8), (2, 3), (5, 5)]
+
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
+
+distances = []
+
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        dist = euclideanDistance(points[i], points[j])
+        distances.append(dist)
+
+min_distance = min(distances)
+print(f"En küçük Öklid mesafesi: {min_distance}")
